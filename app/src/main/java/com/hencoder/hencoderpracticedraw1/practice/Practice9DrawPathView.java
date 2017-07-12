98f27e0f5bf37ca39edb44401fd6bc356fab84b9
chiyoungwu <chiyoungwu@tencent.com>
2017-07-12 17:46:24 +0800

    finish practice 9

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path mPath = new Path();
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -14,6 +19,9 @@ public class Practice9DrawPathView extends View {
 
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPath.addArc(new RectF(200, 200, 400, 400), -225, 225);
+        mPath.arcTo(new RectF(400, 200, 600, 400), 180, 225, false);
+        mPath.lineTo(400, 542);
     }
 
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -25,5 +33,6 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        canvas.drawPath(mPath, mPaint);
     }
 }
