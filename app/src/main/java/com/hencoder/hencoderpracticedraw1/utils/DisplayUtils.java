9de0cf3e1a362bd96a8f3eb0d39b3ff3c8d87bc7
chiyoungwu <chiyoungwu@tencent.com>
2017-07-14 14:47:57 +0800

    add DisplayUtil

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/utils/DisplayUtils.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/utils/DisplayUtils.java
@@ -1,8 +1,25 @@
 package com.hencoder.hencoderpracticedraw1.utils;
 
+import android.content.Context;
+
 /**
  * Created by Nighter on 17/7/14.
  */
 
 public class DisplayUtils {
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
 }
